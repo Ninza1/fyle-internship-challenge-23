bd8c40f1e3a793d2fc550e80eec085a320e00ff5
Iqbal <148518013+wmiqbal@users.noreply.github.com>
2024-05-25 17:49:07 +0530

    commit

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -1,33 +1,4 @@
 
-// import { Injectable } from '@angular/core';
-// import { HttpClient } from '@angular/common/http';
-// import { Observable } from 'rxjs';
-// import { map } from 'rxjs/operators';
-// import { Repository } from '../models/repository.model';
-
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class GithubService {
-//   private apiUrl = 'https://api.github.com/users';
-
-//   constructor(private http: HttpClient) {}
-
-//   getRepositories(username: string, page: number, perPage: number): Observable<Repository[]> {
-//     const url = `${this.apiUrl}/${username}/repos?page=${page}&per_page=${perPage}`;
-//     return this.http.get<Repository[]>(url).pipe(
-//       map((repos: any[]) => repos.map(repo => ({
-//         name: repo.name,
-//         description: repo.description,
-//         topics: repo.topics || []
-//       })))
-//     );
-//   }
-// }
-
-// =============== new ===========
-// src/app/services/github.service.ts
-
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, forkJoin } from 'rxjs';
